c4ff834512a680533ef0e4dfb27b07aed3db63db
Reagan Harper <reagan.harper@trademe.co.nz>
2023-05-04 14:56:24 +1200

    main first commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Stuff</h1>
+  <ul class="links">
+    <li><a href="https://gotocon.us20.list-manage.com/track/click?u=8b4874e78cc832499709a82a2&id=7177ab0ffd&e=e8e3b3ab9b">YOW Talks 2022</a> (Use password <span>GOTO_YOW!-2022</span>)</li>
+  </ul>
+
+</main>
+
+<style>
+  span {
+    color: #ed652e; 
+    background-color: #565657; 
+    border: 1px solid #e1e1e8; 
+    border-radius: 3px; 
+    padding: 2px 1px;
+  }
+  ul {
+    list-style:none;
+    padding-left:0;
+  }
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
